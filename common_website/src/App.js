21f99c2b0795db1ed8e5f6e966ea1a5fa93f2992
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-12 21:12:03 +0530

    Routes setup

--- common_website/src/App.js
+++ common_website/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import NotFound from "./pages/404/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
